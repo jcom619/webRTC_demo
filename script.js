d008996164097c22be5d1977edb97aa6313aeeff
Joshua <joshuacompton@my.smccd.edu>
2022-07-14 13:42:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ window.onload = function() {
 
             // Create an object URL for the video stream and use this 
             // to set the video source.
-            vid.src = window.URL.createObjectURL(localMediaStream);
+            vid.src = window.URL.createObjectURL.localMediaStream;
         },
 
         // Error Callback
